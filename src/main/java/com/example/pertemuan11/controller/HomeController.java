3aca278e8f50d1b725da2e0c474b01237126e30c
taznimt <taznimanwar06@gmail.com>
2026-05-18 20:05:56 +0700

    feat: add home controller to display logged-in user profile

--- src/main/java/com/example/pertemuan11/controller/HomeController.java
+++ src/main/java/com/example/pertemuan11/controller/HomeController.java
@@ -1,4 +1,26 @@
 package com.example.pertemuan11.controller;
 
+import com.example.pertemuan11.model.User;
+import com.example.pertemuan11.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+
 public class HomeController {
+    private final AuthService authService;
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        User user = authService.getLoggedInUser();
+
+        model.addAttribute("user", user);
+        model.addAttribute("profile",user.getProfile());
+
+        return "home";
+
+    }
 }
