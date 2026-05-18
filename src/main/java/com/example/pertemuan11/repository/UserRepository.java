9b4ea2afbcbdf5efc0a173f371c46cac9b5ecd39
taznimt <taznimanwar06@gmail.com>
2026-05-18 10:56:12 +0700

    feat: add User repository with username lookup

--- src/main/java/com/example/pertemuan11/repository/UserRepository.java
+++ src/main/java/com/example/pertemuan11/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.example.pertemuan11.repository;
 
-public interface UserRepository {
+import com.example.pertemuan11.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUsername(String username);
 }
