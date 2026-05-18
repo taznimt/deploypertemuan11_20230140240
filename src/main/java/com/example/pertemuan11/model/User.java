85b32b8fbed87a560b4b7d6218d9813bfcde127b
taznimt <taznimanwar06@gmail.com>
2026-05-18 10:50:50 +0700

    feat: add User entity with Profile relationship

--- src/main/java/com/example/pertemuan11/model/User.java
+++ src/main/java/com/example/pertemuan11/model/User.java
@@ -1,4 +1,23 @@
 package com.example.pertemuan11.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class User {
+    @Id
+    private String id;
+    private String username;
+    private String password;
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Profile profile;
 }
