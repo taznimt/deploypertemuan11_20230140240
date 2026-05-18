c037f3fa69af7bfadb9e5c7b3b88dcde5d1c5437
taznimt <taznimanwar06@gmail.com>
2026-05-18 11:37:00 +0700

    refactor: add Builder annotation to Profile entity

--- src/main/java/com/example/pertemuan11/model/Profile.java
+++ src/main/java/com/example/pertemuan11/model/Profile.java
@@ -2,11 +2,13 @@ package com.example.pertemuan11.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Builder
 @Table(name = "profile")
 @AllArgsConstructor
 @NoArgsConstructor
