9893fd29ad9c2aadea8e2dbf4aef6e02f74e3561
taznimt <taznimanwar06@gmail.com>
2026-05-18 10:59:07 +0700

    feat: add register request DTO

--- src/main/java/com/example/pertemuan11/model/dto/RegisterRequest.java
+++ src/main/java/com/example/pertemuan11/model/dto/RegisterRequest.java
@@ -1,4 +1,14 @@
 package com.example.pertemuan11.model.dto;
 
+import lombok.Data;
+
+@Data
+
 public class RegisterRequest {
+    private String username;
+    private String password;
+
+    private String nama;
+    private String alamat;
+
 }
