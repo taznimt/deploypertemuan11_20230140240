83971dd6c69e15eccb67da96c80302fc1b9b5e58
taznimt <taznimanwar06@gmail.com>
2026-05-18 10:45:20 +0700

    Add Profile entity with one-to-one User relationship

--- src/main/java/com/example/pertemuan11/model/Profile.java
+++ src/main/java/com/example/pertemuan11/model/Profile.java
@@ -1,4 +1,23 @@
 package com.example.pertemuan11.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "profile")
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Profile {
+    @Id
+    private String id;
+    private String nama;
+    private String alamat;
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
 }
